9ae331287163fd59cc4a6c37e4636cc5256f1167
luk3yx <luk3yx@users.noreply.github.com>
2018-07-25 07:12:37 +1200

    Replace minetest.chat_send_player() with return

--- overrides.lua
+++ overrides.lua
@@ -3,9 +3,9 @@ local oldKill = minetest.registered_chatcommands["killme"].func
 minetest.override_chatcommand("killme", {
 	func = function(name, param)
 		if knockout.knocked_out[name] == nil then
-			oldKill(name, param)
+			return oldKill(name, param)
 		else
-			minetest.chat_send_player(name, "You can't kill yourself!")
+			return false, "You can't kill yourself!"
 		end
 	end
 })
